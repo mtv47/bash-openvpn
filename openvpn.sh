d952c435859a4a47b1faac98be367c76fb86cf6c
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-09-05 16:05:31 +0200

    fixes

--- openvpn.sh
+++ openvpn.sh
@@ -2,17 +2,17 @@
 
 echo "================================================================================"
 echo "Install dependencies"
-sudo apt install ca-certificates wget net-tools gnupg
+sudo apt install ca-certificates wget net-tools gnupg -y
 
 echo "================================================================================"
 echo "Add OpenVPN repository"
-sudo wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
+sudo wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | sudo apt-key add -
 sudo echo "deb http://as-repository.openvpn.net/as/debian focal main">/etc/apt/sources.list.d/openvpn-as-repo.list
 sudo apt update
 
 echo "================================================================================"
 echo "Install OpenVPN"
-sudo apt install openvpn-as
+sudo apt install openvpn-as -y
 
 echo "================================================================================"
 echo "Setting up a password for the WebUI"
